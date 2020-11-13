6fbe96ff21ad0c7fe97012a03db1a8072aac3f66
Emma VANDECASTEELE <etu40268@henallux.be>
2020-11-13 16:46:26 +0100

    Initial commit

---
+++ src/main/java/com/webshop/sportnutrition/SportnutritionApplication.java
@@ -0,0 +1,13 @@
+package com.webshop.sportnutrition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportnutritionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportnutritionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webshop/sportnutrition/SportnutritionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webshop.sportnutrition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportnutritionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

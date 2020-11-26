9b054eb2649caad8fd5994fca46f29a135503e1b
etu41834 <etu41834@henallux.be>
2020-11-26 15:15:14 +0100

    Add getters and setters in Customer

--- src/main/java/com/webshop/sportnutrition/dataAccess/entity/CustomerEntity.java
+++ src/main/java/com/webshop/sportnutrition/dataAccess/entity/CustomerEntity.java
@@ -49,4 +49,112 @@ public class CustomerEntity {
 
     @Column(name="country")
     private String country;
+
+    /* ------ GETTERS ------ */
+
+    public Integer getCustomerID() {
+        return customerID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getNbFidelityPoints() {
+        return nbFidelityPoints;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public Integer getZipCode() {
+        return zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    /* ------ SETTERS ------ */
+
+    public void setCustomerID(Integer customerID) {
+        this.customerID = customerID;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setNbFidelityPoints(Integer nbFidelityPoints) {
+        this.nbFidelityPoints = nbFidelityPoints;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public void setZipCode(Integer zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }

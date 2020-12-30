6a826704f5c512619a25785408e9dd5294e6a9ec
Emma VANDECASTEELE <etu40268@henallux.be>
2020-12-30 20:39:28 +0100

    save categories into session to avoid a loading from database for each pages

--- src/main/java/com/webshop/sportnutrition/controller/MasterController.java
+++ src/main/java/com/webshop/sportnutrition/controller/MasterController.java
@@ -6,38 +6,58 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
 import java.util.ArrayList;
 import java.util.Locale;
 
+@SessionAttributes({MasterController.FRCATS, MasterController.ENCATS})
 public class MasterController {
+
+    protected static final String FRCATS="frCategories";
+    protected static final String ENCATS="enCategories";
+
 
     @Autowired
     private TranslationDataAccess translationDAO;
 
     public MasterController(){}
 
-    @ModelAttribute
-    public void everyRequest(Model model) {
-
-        /*Locale french = Locale.FRENCH;
-        Locale currentLang = LocaleContextHolder.getLocale();
+    @ModelAttribute(FRCATS)
+    public ArrayList<Translation> addFrenchCategoriesIntoSessionScope() {
 
         Language lang = new Language();
-        if (currentLang == french)
-            lang.setLanguageID(1);
-        else
-            lang.setLanguageID(2);*/
+        lang.setLanguageID(1);
+
+        return translationDAO.getCategoryByLanguage(lang);
+    }
+
+    @ModelAttribute(ENCATS)
+    public ArrayList<Translation> addEnglishCategoriesIntoSessionScope() {
+
+        Language lang = new Language();
+        lang.setLanguageID(2);
+
+        return translationDAO.getCategoryByLanguage(lang);
+    }
+
+    @ModelAttribute
+    public void everyRequest(Model model,
+                             @ModelAttribute(value=FRCATS) ArrayList<Translation> frTranslations,
+                             @ModelAttribute(value=ENCATS) ArrayList<Translation> enTranslations) {
 
         Locale english = Locale.ENGLISH;
         Locale currentLang = LocaleContextHolder.getLocale();
 
-        Language lang = new Language();
-        if (currentLang == english)
-            lang.setLanguageID(2);
-        else
-            lang.setLanguageID(1);
+        ArrayList<Translation> translations;
 
-        ArrayList<Translation> translations = translationDAO.getCategoryByLanguage(lang);
+        Language lang = new Language();
+        if (currentLang == english) {
+                translations = enTranslations;
+            }
+        else {
+                translations = frTranslations;
+        }
 
         for (Translation translation : translations) {
             switch (translation.getCategory().getCategoryID()) {
